8dde1dedea28c5ff8abb8d71042919ff41c9449b
RonRain <rarainero@gmail.com>
2023-09-03 12:20:30 -0400

    add button and message constants under cached element references

--- js/app.js
+++ js/app.js
@@ -8,7 +8,12 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
+const messageEl = document.querySelector(".msg")
+const buttonElOne = document.querySelector(".btn-1")
+const buttonElTwo = document.querySelector(".btn-2")
+const buttonElThree = document.querySelector(".btn-3")
 
+console.log(buttonElOne)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
