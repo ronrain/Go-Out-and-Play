90824ae1ad563b5f076f2c6afc9f0205a27c1e35
RonRain <rarainero@gmail.com>
2023-09-05 22:44:27 -0400

    update adventure story

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const messageEl = document.querySelector(".msg")
 const optionElOne = document.querySelector(".btn-1")
 const optionElTwo = document.querySelector(".btn-2")
 const optionElThree = document.querySelector(".btn-3")
+const adventStory = document.getElementById("adventImage")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -39,25 +40,23 @@ optionElThree.textContent=(adventEls[currentIndex].optionThree)
 }
 
 function handleClick(evt){
-  currentIndex = parseInt(evt.target.id) 
-  const currentElement = adventEls[currentIndex]
+currentIndex = parseInt(evt.target.id) 
+const currentElement = adventEls[currentIndex]
   if (!currentElement.optionOne){
     optionElOne.style.display = "none"
   } else {
-  optionElOne.style.display = "inline"
-}
-
-if (!currentElement.optionTwo){
-  optionElTwo.style.display = "none"
-} else {
-optionElTwo.style.display = "inline"
-}
-
-if (!currentElement.optionThree){
-optionElThree.style.display = "none"
-} else {
-optionElThree.style.display = "inline"
-}
+    optionElOne.style.display = "inline"
+  }
+  if (!currentElement.optionTwo){
+    optionElTwo.style.display = "none"
+  } else {
+    optionElTwo.style.display = "inline"
+  }
+  if (!currentElement.optionThree){
+    optionElThree.style.display = "none"
+  } else {
+    optionElThree.style.display = "inline"
+  }
   render()
 }
 
@@ -68,5 +67,8 @@ function render(){
   optionElTwo.id = adventEls[currentIndex].optionTwoResult || 'none'
   optionElThree.textContent = adventEls[currentIndex].optionThree
   optionElThree.id = adventEls[currentIndex].optionThreeResult || 'none'
+  
   messageEl.textContent = adventEls[currentIndex].optionText
+
+  adventImage.src = adventEls[currentIndex].imageEl
 }
